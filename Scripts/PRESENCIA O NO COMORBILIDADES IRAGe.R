e8a76587d7247832f81a69efc77021175d91d3e3
anaveronicadigiovanni <anaveronicadigiovanni@gmail.com>
2026-04-25 23:52:34 -0300

    Agrego scrpts tortas comorbilidades o no en IRAG e IRAGe

---
+++ Scripts/PRESENCIA O NO COMORBILIDADES IRAGe.R
@@ -0,0 +1,31 @@
+##########presencia comorbilidades IRAGE#
+
+##1- Creo la tabla#
+PRES_COMORB_IRAGE<-data_irage |>
+  group_by(PRESENCIA_COMORBILIDADES)%>% 
+  summarise(CASOS = n())%>%
+  ungroup()
+
+# 2. CreO el gráfico de TORTA###
+torta_comorbilidades_IRAGE<-highchart() %>%
+  hc_chart(type = "pie") %>%
+  hc_add_series(
+    data = PRES_COMORB_IRAGE,
+    type = "pie",
+    hcaes(name = PRESENCIA_COMORBILIDADES, y = CASOS),
+    name = "Proporción",
+    colorByPoint = TRUE
+  ) %>%
+  hc_plotOptions(
+    pie = list(
+      dataLabels = list(
+        enabled = TRUE,
+        # Formato: Nombre (Porcentaje sin decimales)
+        format = '{point.name}: {point.percentage:.0f}%' 
+      )
+    )
+  ) %>%
+  hc_title(text = "Presencia de comorbilidades en los casos de IRAGe") %>%
+  hc_tooltip(pointFormat = "<b>{point.y}</b> casos<br/>{point.percentage:.1f}%")
+
+torta_comorbilidades_IRAGE
