fc7ea25043acea0d143be68883785291224f28b8
mercedespaz <pazmercedes.mp@gmail.com>
2026-05-04 11:57:17 -0300

    Modifico encoding en la importación de base nominal.

--- Scripts/Importar base.R
+++ Scripts/Importar base.R
@@ -1,5 +1,7 @@
 ##LLAMAR BASE CON CODIGO Q LEA ACENTOS
 
-data <- read.csv2("bases/UC_IRAG_CATAMARCA.csv", sep=";", encoding="latin1")
+data <- read.csv("bases/UC_IRAG_CATAMARCA.csv", sep=";")
 
+#Modifico encoding a UTF 8 para evitar incovenientes con el source
+data$CLASIFICACION_MANUAL <- iconv(data$CLASIFICACION_MANUAL, from = "latin1", to = "UTF-8")
 
