e0aa0b65ede882e81651932f624bd1196665ed61
anaveronicadigiovanni <anaveronicadigiovanni@gmail.com>
2026-05-17 22:47:29 -0300

    Saco el título y unifico theme con lo demás

--- Scripts/agrupados.R
+++ Scripts/agrupados.R
@@ -12,28 +12,27 @@
      ungroup()
 
  # 2. Crear el gráfico de columnas al 100%
-  Grafico_virus_columnas<- highchart() %>%
-     hc_chart(type = "column") %>%
-     hc_plotOptions(column = list(stacking = "percent")) %>% # <--- Esto hace que sumen 100%
-    hc_add_series(
-        data = dfagrupados, 
-        type = "column", 
-        hcaes(x = SEMANA, y = Total, group = NOMBREEVENTOAGRP)
-      ) %>%
-    hc_title(text = "Proporción de IRAG e IRAG Extendida sobre el Total de Internados") %>%
-    hc_xAxis(title = list(text = "Semana Epidemiológica")) %>%
-   hc_yAxis(title = list(text = "Porcentaje (%)"), max = 100) %>%
-    hc_credits(text = enc2utf8("Fuente: Elaboración propia en base a datos del SNVS 2.0"), 
-               enabled = TRUE) %>% 
-   hc_tooltip(
-       shared = TRUE,
-        pointFormat = '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.percentage:.1f}%</b> ({point.y} casos)<br/>'
-       ) %>%
-    hc_add_theme(hc_theme_gridlight())  
-  
-  # 3. LLAMO AL GRÁFICO
-  
-  Grafico_virus_columnas
+  # Grafico_virus_columnas<- highchart() %>%
+  #    hc_chart(type = "column") %>%
+  #    hc_plotOptions(column = list(stacking = "percent")) %>% # <--- Esto hace que sumen 100%
+  #   hc_add_series(
+  #       data = dfagrupados, 
+  #       type = "column", 
+  #       hcaes(x = SEMANA, y = Total, group = NOMBREEVENTOAGRP)
+  #     ) %>%
+  #   hc_title(text = "Proporción de IRAG e IRAG Extendida sobre el Total de Internados") %>%
+  #   hc_xAxis(title = list(text = "Semana Epidemiológica")) %>%
+  #  hc_yAxis(title = list(text = "Porcentaje (%)"), max = 100) %>%
+  #   hc_credits(text = enc2utf8("Fuente: Elaboración propia en base a datos del SNVS 2.0"), 
+  #              enabled = TRUE) %>% 
+  #  hc_tooltip(
+  #      shared = TRUE,
+  #       pointFormat = '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.percentage:.1f}%</b> ({point.y} casos)<br/>'
+  #      )
+  # #%>%
+  #  # hc_add_theme(hc_theme_gridlight())  
+  # 
+
   
   Grafico_virus_columnas <- highchart() %>%
     hc_chart(type = "column") %>%
@@ -46,7 +45,7 @@
     # Define aquí tus colores (puedes usar nombres o códigos Hex)
     # Cambia el orden según cuál serie quieres que sea gris
     hc_colors(c("#7cb5ec", "#434399","#D3D3D3" )) %>% 
-    hc_title(text = "Proporción de IRAG e IRAG Extendida sobre el Total de Internados") %>%
+    #hc_title(text = "Proporción de IRAG e IRAG Extendida sobre el Total de Internados") %>%
     hc_xAxis(title = list(text = "Semana Epidemiológica")) %>%
     hc_yAxis(title = list(text = "Porcentaje (%)"), max = 100) %>%
     hc_credits(text = enc2utf8("Fuente: Elaboración propia en base a datos del SNVS 2.0"), 
@@ -54,6 +53,11 @@
     hc_tooltip(
       shared = TRUE,
       pointFormat = '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.percentage:.1f}%</b> ({point.y} casos)<br/>'
-    ) %>%
-    hc_add_theme(hc_theme_gridlight())
+    ) 
+ # %>%
+  #  hc_add_theme(hc_theme_gridlight())
+  
+  # 3. LLAMO AL GRÁFICO
+  
+  Grafico_virus_columnas
    
